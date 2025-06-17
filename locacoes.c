9f7d77222b20afa1b620a8e6714efc37333fec3f
Livlivm <liviaampereira@gmail.com>
2025-06-17 11:30:44 -0300

    Update locacoes.c

--- locacoes.c
+++ locacoes.c
@@ -1,7 +1,4 @@
 #include <stdio.h>
-#include <stdlib.h>
-
-#include "clientes.h"
 #include "locacoes.h"
 
 void realizarLocacao(Locacao *locacoes, int *totalLocacoes, Cliente *clientes, int totalClientes) {
@@ -14,21 +11,30 @@ void realizarLocacao(Locacao *locacoes, int *totalLocacoes, Cliente *clientes, i
     printf("4 - Drama\n");
     printf("5 - Sair\n");
     scanf("%d", &genero);
+    
+    printf("Digite o ID do cliente: \n");
+    scanf("%d", &locacoes[*totalLocacoes].IDCliente);
+
+    printf("Digite o nome do filme: \n");
+    scanf("%s", locacoes[*totalLocacoes].Filme);
+
+    (*totalLocacoes)++;
+    printf("Locação realizada com sucesso!\n");
 }
+
 void listarLocacoes(Locacao *locacoes, int totalLocacoes) {
     printf("\n ---== Listar Locações ==---\n");
     for (int i = 0; i < totalLocacoes; i++) {
         printf("ID Cliente: %d\n", locacoes[i].IDCliente);
         printf("Filme: %s\n", locacoes[i].Filme);
-        printf("Data: %s\n", locacoes[i].Data);
     }
 }
+
 void buscarLocacoesPorCliente(Locacao *locacoes, int totalLocacoes, int idCliente) {
     printf("\n---== Buscar Locações por Cliente ==---\n");
     for (int i = 0; i < totalLocacoes; i++) {
         if (locacoes[i].IDCliente == idCliente) {
             printf("Filme: %s\n", locacoes[i].Filme);
-            printf("Data: %s\n", locacoes[i].Data);
         }
     }
 }
