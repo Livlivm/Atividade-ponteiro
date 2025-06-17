621aa774f03eaddd9b2b27bb0a0e9bfd0383466f
Livlivm <liviaampereira@gmail.com>
2025-06-17 11:30:06 -0300

    Update locacoes.h

--- locacoes.h
+++ locacoes.h
@@ -1,13 +1,15 @@
 #ifndef LOCACOES_H
 #define LOCACOES_H
 
+#include "clientes.h"
+
 typedef struct {
     int IDCliente;
     char Filme[50];
-    char Data[11]; 
 } Locacao;
 
 void realizarLocacao(Locacao *locacoes, int *totalLocacoes, Cliente *clientes, int totalClientes);
 void listarLocacoes(Locacao *locacoes, int totalLocacoes);
 void buscarLocacoesPorCliente(Locacao *locacoes, int totalLocacoes, int idCliente);
+
 #endif
